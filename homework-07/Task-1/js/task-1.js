a54e41703bb032a4884f0a90f292f7adcf0d21a2
DRAKOS <drakostius2@gmail.com>
2020-06-24 00:59:16 +0300

    Finished task6 of HW-7

--- homework-07/Task-1/js/task-1.js
+++ homework-07/Task-1/js/task-1.js
@@ -1,13 +1,15 @@
-const amountOfClasses = (className) => {
-  //   return document.querySelectorAll(className).length;
-  return document.querySelectorAll(className).length;
+const categoryListDescpition = function (mainList) {
+  console.log(`В списке ${mainList.children.length} категории`);
+
+  Array.from(mainList.children).forEach((element) => {
+    const categoryName = element.querySelector("h2,h3,h4").textContent;
+    const subCategoryList = Array.from(element.querySelectorAll("li"));
+
+    console.log("Категория: " + categoryName);
+    console.log("Количество элементов: " + subCategoryList.length);
+  });
 };
 
-const amountOfCategories = amountOfClasses("li.item");
-const amountOfSubCategories = amountOfClasses("li.item ul li");
+const allLists = document.querySelector("#categories");
 
-console.log(`В списке ${amountOfCategories} категории`);
-console.log(`В списке ${amountOfSubCategories} категории`);
-
-// const allCategories =
-// const subCategories
+categoryListDescpition(allLists);
